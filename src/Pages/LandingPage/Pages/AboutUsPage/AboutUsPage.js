6e81abafce15eadf0138ad6b1d228f52dc54e0d3
tarun5602 <tarunshrivastav400@gmail.com>
2025-03-23 12:12:36 +0530

    AboutUs Page

--- src/Pages/LandingPage/Pages/AboutUsPage/AboutUsPage.js
+++ src/Pages/LandingPage/Pages/AboutUsPage/AboutUsPage.js
@@ -1,11 +1,131 @@
-import React from 'react'
+import React from "react";
 import "./styles.css";
+import ASSETS from "../../../../assets";
 
 export default function AboutUsPage() {
+  const Mission = [
+    {
+      id: 1,
+      title: 25,
+      description: "Years of Experience",
+      icon: "",
+    },
+    {
+      id: 2,
+      title: 500,
+      description: "Destination",
+      icon: "",
+    },
+    {
+      id: 3,
+      title: "1M",
+      description: "Happy Customers",
+      icon: "",
+    },
+    {
+      id: 4,
+      title: 1000,
+      description: "Flight Routes",
+      icon: "",
+    },
+  ];
+
+  const Values = [
+    {
+      id: 1,
+      title: "Saftey First",
+      description:
+        "Your safety is our top priority. We maintain the highest standards in aviation safety.",
+      icon: "",
+    },
+    {
+      id: 2,
+      title: "Customer  Care",
+      description:
+        "We are committed to providing exceptional service at every step of your journey.",
+      icon: "",
+    },
+    {
+      id: 3,
+      title: "Global Reach",
+      description:
+        "Connect to over 500 destinations worldwide with our extensive network.",
+      icon: "",
+    },
+  ];
   return (
-    <div>
-    r4fr
-      
+    <div className="aboutUsBaseContainer">
+      <section
+        className="aboutUsImageBaseContainer"
+        style={{ backgroundImage: `url(${ASSETS.aboutUsPageImage})` }}
+      >
+        <div className="aboutUsInfoContainer">
+          <h1
+            style={{
+              color: "var(--whiteColor)",
+            }}
+          >
+            About Us
+          </h1>
+        </div>
+      </section>
+      <section className="aboutUsMissionBaseContainer">
+        <div className="aboutUsMissionTextContainer">
+          <h1
+            style={{
+              color: "var(--baseColor)",
+              padding: "50px",
+            }}
+          >
+            Our Mission
+          </h1>
+          <p
+            style={{
+              color: "var(--grayColor)",
+              paddingRight: "100px",
+              paddingLeft: "100px",
+              paddingBottom: "80px",
+              textAlign: "justify",
+            }}
+          >
+            At SkyWings, we are more than just an airline. We are your trusted
+            partner in creating unforgettable travel experiences. Our mission is
+            to connect people and places with comfort, reliability, and
+            sustainability at the heart of everything we do.
+          </p>
+        </div>
+        <div className="aboutUsMissionAnimationContainer">
+          {Mission.map((item) => (
+            <div key={item.id} className="aboutUsMissionCardContainer">
+              <div className="aboutUsMissionTitle">{item.title}</div>
+              <div className="aboutUsMissionDescription">
+                {item.description}
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="aboutUsOurValuesBaseContainer">
+        <div className="aboutUsValuesHeading">
+          <h1>Our Values</h1>
+        </div>
+        <div className="aboutUsValuesCardContainer">
+          {Values.map((item) => (
+            <div key={item.id} className="aboutUsValuesContainer">
+              <div className="aboutUsValueIcon">
+                {item.icon}
+              </div>
+              <div className="aboutUsValueTitle">
+                {item.title}
+              </div>
+              <div className="aboutUsValueDescription">
+                {item.description}
+              </div>
+            </div>
+          )
+        )}
+        </div>
+      </section>
     </div>
-  )
+  );
 }
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,11 @@
 const ASSETS = {
   loginBackImage: require("./images/loginBackImage.jpg"),
-  registerBackImage: require("./images/registerbackImage.jpg")
+  registerBackImage: require("./images/registerbackImage.jpg"),
+  aboutUsPageImage: require("./images/aboutUsImage.jpg"),
+  destinationParisImage: require("./images/destinationParisImage.jpg"),
+  destinationSantoriniImage: require("./images/destinationSantoriniImage.jpg"),
+  destinationTokyoImage: require("./images/destinationTokyoImage.jpg"),
+  destinationBaliImage: require("./images/destinationBaliImage.jpg"),
 };
 
 export default ASSETS;
