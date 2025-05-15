90f7edf0ddc333f6676dc65ebe591df8d19d55c4
shobit002 <shobitmahajan99@gmail.com>
2025-05-15 15:07:12 +0530

    minor changes

--- src/Pages/LandingPage/Pages/AboutUsPage/AboutUsPage.js
+++ src/Pages/LandingPage/Pages/AboutUsPage/AboutUsPage.js
@@ -10,19 +10,19 @@ export default function AboutUsPage() {
   const Mission = [
     {
       id: 1,
-      title: 25,
+      title: 2 ,
       description: "Years of Experience",
       icon: "",
     },
     {
       id: 2,
-      title: 500,
+      title: 25,
       description: "Destination",
       icon: "",
     },
     {
       id: 3,
-      title: "1M",
+      title: "10k+",
       description: "Happy Customers",
       icon: "",
     },
@@ -47,14 +47,14 @@ export default function AboutUsPage() {
       title: "Customer  Care",
       description:
         "We are committed to providing exceptional service at every step of your journey.",
-      icon: <LuHeartHandshake size={35}/>,
+      icon: <LuHeartHandshake size={35} />,
     },
     {
       id: 3,
       title: "Global Reach",
       description:
         "Connect to over 500 destinations worldwide with our extensive network.",
-      icon: <HiOutlineGlobe size={35}/>,
+      icon: <HiOutlineGlobe size={35} />,
     },
   ];
   return (
@@ -71,6 +71,21 @@ export default function AboutUsPage() {
           >
             About Us
           </h1>
+          <p
+            style={{
+              color: "var(--grayColor)",
+              paddingRight: "100px",
+              paddingLeft: "100px",
+              paddingBottom: "80px",
+              textAlign: "justify",
+              fontSize: "20px",
+            }}
+          >
+            "We believe every journey should feel effortless — our Airline
+            Management System is designed to care for every passenger, with
+            intuitive technology that puts comfort, clarity, and connection
+            first."
+          </p>
         </div>
       </section>
       <section className="aboutUsMissionBaseContainer">
@@ -93,9 +108,10 @@ export default function AboutUsPage() {
               fontSize: "20px",
             }}
           >
-            At SkyWings, we are more than just an airline. We are your trusted
+            At AirWings, we are more than just an airline. We are your trusted
             partner in creating unforgettable travel experiences. Our mission is
-            to connect people and places with comfort and exciting experiences at the heart of everything we do.
+            to connect people and places with comfort and exciting experiences
+            at the heart of everything we do.
           </p>
         </div>
         <div className="aboutUsMissionAnimationContainer">
@@ -116,21 +132,16 @@ export default function AboutUsPage() {
         <div className="aboutUsValuesCardContainer">
           {Values.map((item) => (
             <div key={item.id} className="aboutUsValuesContainer">
-              <div className="aboutUsValueIcon">
-                {item.icon}
-              </div>
+              <div className="aboutUsValueIcon">{item.icon}</div>
               <div className="aboutUsValueTitle">
                 <h1>{item.title}</h1>
               </div>
-              <div className="aboutUsValueDescription">
-                {item.description}
-              </div>
+              <div className="aboutUsValueDescription">{item.description}</div>
             </div>
-          )
-        )}
+          ))}
         </div>
       </section>
-      <CustomFooter/>
+      <CustomFooter />
     </div>
   );
 }
